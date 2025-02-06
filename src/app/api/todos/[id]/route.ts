a545e6b080089e6920db004f94cf027b89e70318
Santiago Cecconato <santycheco1@gmail.com>
2025-02-06 13:11:37 -0300

    fixes

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,73 +1,67 @@
-import prisma from "@/lib/prisma";
-import { NextResponse, NextRequest } from "next/server";
+import prisma from '@/lib/prisma';
+import { Todo } from '@prisma/client';
+import { NextResponse, NextRequest } from 'next/server';
 import * as yup from 'yup';
-import {Todo} from '@prisma/client';
-
 
 interface Segments {
-    params: {
-        id: string;
-    }
+  params: {
+    id: string;
+  }
 }
 
-const getTodo = async(id:string):Promise<Todo | null> => {
+const getTodo = async( id: string ):Promise<Todo | null> => {
 
-    const todo = await prisma.todo.findFirst({
-        where: {
-            id: id
-        }
-    });
+  const todo = await prisma.todo.findFirst({ where: { id } });
 
-    return todo;
+  return todo;
 }
 
-export async function GET(req: NextRequest, { params }: Segments) {
-    
-    const todo = await getTodo(params.id);
-
-    if (!todo) {
-        return NextResponse.json({
-            message: "todo no encontrado"
-        }, {
-            status: 404
-        });
-    }
 
 
-    return NextResponse.json(todo);
+
+export async function GET(request: Request, { params }: Segments ) { 
+
+  
+  const todo = await getTodo(params.id);
+
+  if ( !todo ) {
+    return NextResponse.json({ message: `Todo con id ${ params.id } no exite` }, { status: 404 });
+  }
+
+
+  return NextResponse.json(todo);
 }
+
+
 
 const putSchema = yup.object({
-    complete: yup.boolean().optional(),
-    description: yup.string().optional()
+  complete: yup.boolean().optional(),
+  description: yup.string().optional(),
 })
 
-export async function PUT(req: NextRequest, { params }: Segments) {
+export async function PUT(request: NextRequest, { params }: Segments ) { 
+
+  
+  const todo = await getTodo(params.id);
+
+  if ( !todo ) {
+    return NextResponse.json({ message: `Todo con id ${ params.id } no exite` }, { status: 404 });
+  }
+
+  try {
+    const { complete, description } =  await putSchema.validate( await request.json() );
+  
+  
+    const updatedTodo = await prisma.todo.update({
+      where: { id: params.id },
+      data: { complete, description }
+    })
+  
+  
+  
+    return NextResponse.json(updatedTodo);
     
-    const todo = await getTodo(params.id);
-
-
-    if (!todo) {
-        return NextResponse.json({
-            message: "todo no encontrado"
-        }, {
-            status: 404
-        });
-    }
-
-    try{
-        const { complete, description} = await putSchema.validate( await req.json());
-
-        const updatedTodo = await prisma.todo.update({
-            where: { id:params.id } ,
-            data: { complete, description }
-        });    
-    
-    
-        return NextResponse.json(updatedTodo);
-    }catch (error) {
-        return NextResponse.json(error, {
-            status: 400
-        });
-    }
+  } catch (error) {
+    return NextResponse.json(error, { status: 400 });
+  }
 }
--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,65 +1,59 @@
-import { NextResponse, NextRequest } from "next/server";
-import prisma from "@/lib/prisma";
+import prisma from '@/lib/prisma'
+import { NextResponse, NextRequest } from 'next/server'
 import * as yup from 'yup';
 
-export async function GET(req: NextRequest) {
+export async function GET(request: Request) { 
+  
+  const { searchParams } = new URL(request.url)
+  const take = Number( searchParams.get('take') ?? '10');
+  const skip = Number( searchParams.get('skip') ?? '0');
 
-    const { searchParams } = new URL(req.url);
-    const take = +(searchParams.get('take') ?? '10'); //conversion a numero con +
-    const skip = +(searchParams.get('skip') ?? '0');
+  if ( isNaN(take) ){ 
+    return NextResponse.json({ message: 'Take tiene que ser un número' }, { status: 400 });
+  }
 
+  if ( isNaN(skip) ){ 
+    return NextResponse.json({ message: 'Skip tiene que ser un número' }, { status: 400 });
+  }
 
-    //verifico que el parametro take sea un número
-    if (isNaN(take)) {
-        return NextResponse.json({
-            message: "take debe ser un número"
-        }, {
-            status: 400
-        });
-    }
-     //verifico que el parametro skip sea un número
-     if (isNaN(skip)) {
-        return NextResponse.json({
-            message: "skip debe ser un número"
-        }, {
-            status: 400
-        });
-    }
+  const todos = await prisma.todo.findMany({
+    take: take,
+    skip: skip,
+  });
 
-    const todos = await prisma.todo.findMany({
-        take,
-        skip,
-        orderBy: {
-            id: 'desc'
-        }
-    });
-
-    console.log("todos : ");
-    console.log(todos);
-
-    return NextResponse.json(todos);
+  return NextResponse.json( todos );
 }
 
-//YUP
+
 
 const postSchema = yup.object({
-    description: yup.string().required(),
-    complete: yup.boolean().optional().default(false), 
-})
+  description: yup.string().required(),
+  complete: yup.boolean().optional().default(false),
+});
 
 export async function POST(request: Request) { 
-    try {
-        const body = await postSchema.validate(await request.json());
 
-        const todo = await prisma.todo.create({
-            data: body
-        });
+  try {
+    const { complete, description } = await postSchema.validate( await request.json() );
 
-        return NextResponse.json(todo);
-    }catch (error) {
-        return NextResponse.json(error, {
-            status: 400
-        });
-    }
+    const todo = await prisma.todo.create({ data: { complete, description } })
+  
+    
+    return NextResponse.json(todo);
+    
+  } catch (error) {
+    return NextResponse.json( error, { status: 400 } );
+  }
+}
+export async function DELETE(request: Request) { 
+
+  try {
+
+    await prisma.todo.deleteMany({ where: { complete: true } });
+    return NextResponse.json('Borrados');
+    
+  } catch (error) {
+    return NextResponse.json( error, { status: 400 } );
+  }
 }
 
